697cfb1466578339766d500782b4ec7c454d7b20
yingyuelou@yeah.net <qazwsx1232>
2016-12-10 10:24:56 +0800

    1

--- src/main/java/spittr/web/SpitterController.java
+++ src/main/java/spittr/web/SpitterController.java
@@ -3,11 +3,14 @@ package spittr.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import spittr.Spitter;
 import spittr.data.SpitterRepository;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/spitter")
@@ -24,16 +27,16 @@ public class SpitterController {
   public String showRegistrationForm(){
    return "registerForm";
  }
- @RequestMapping(value = "/register",method = RequestMethod.POST)
+ /*@RequestMapping(value = "/register",method = RequestMethod.POST)
     public String processRegistration(Spitter spitter){
      spitterRepository.save(spitter);
-     return "redirect:/spitter"+spitter.getUsername(); }
+     return "redirect:/spitter"+spitter.getUsername(); }*/
      //重定向到基本页信息
      /*当InternalResourceViewResolver看到视图格式中的“redirect:”前缀时，它就知道要将
      其解析为重定向的规则，而不是视图的名称。在本例中，它将会重定向到用户基本信息的页
      面。例如，如果Spitter.username属性的值为“jbauer”，那么视图将会重定向
      到“/spitter/jbauer”。*/
- /*@RequestMapping(value="/register", method=RequestMethod.POST)
+ @RequestMapping(value="/register", method=RequestMethod.POST)
  public String processRegistration(
          @Valid Spitter spitter,
          Errors errors) {
@@ -43,7 +46,7 @@ public class SpitterController {
 
      spitterRepository.save(spitter);
      return "redirect:/spitter/" + spitter.getUsername();
- }*/
+ }
 
     @RequestMapping(value="/{username}", method=RequestMethod.GET)
     public String showSpitterProfile(@PathVariable String username, Model model) {
